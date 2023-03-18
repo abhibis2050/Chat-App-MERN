877c3d2c6857509ae7aa19250f2955ad1a27c5b3
Abhisek Biswas <techsupport@drivado.com>
2023-03-18 18:56:38 +0530

    fourth commit

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -1,6 +1,7 @@
-import { useToast } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+import { Box, Button, useToast } from "@chakra-ui/react";
 import axios from "axios";
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { ChatState } from "../Context/ChatProvider";
 
 const MyChats = () => {
@@ -16,7 +17,10 @@ const MyChats = () => {
           Authorization: `Bearer ${user.token}`,
         },
       };
-      const { data } = await axios.get("http://localhost:5000/api/chat", config);
+      const { data } = await axios.get(
+        "http://localhost:5000/api/chat",
+        config
+      );
       // console.log(data);
       setChats(data);
     } catch (error) {
@@ -31,11 +35,56 @@ const MyChats = () => {
     }
   };
 
-  useEffect(()=>{
-    setLoggedUser(JSON.parse(localStorage.getItem("userInfo")))
-    fetchChats()
-  },[])
-  return <div>MyChats</div>;
+  useEffect(() => {
+    setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
+    fetchChats();
+  }, []);
+  return (
+    <Box
+      display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      flexDir="column"
+      alignItems="center"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "31%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <Box
+      pb={3}
+      px={3}
+      fontSize={{ base: "28px", md: "30px" }}
+      fontFamily="Work sans"
+      display="flex"
+      w="100%"
+      justifyContent="space-between"
+      alignItems="center"
+      >
+        My Chats
+        {/* <GroupChatModal> */}
+          <Button
+            d="flex"
+            fontSize={{ base: "17px", md: "10px", lg: "17px" }}
+            rightIcon={<AddIcon />}
+          >
+            New Group Chat
+          </Button>
+        {/* </GroupChatModal> */}
+      </Box>
+      <Box
+       display="flex"
+       flexDir="column"
+       p={3}
+       bg="#F8F8F8"
+       w="100%"
+       h="100%"
+       borderRadius="lg"
+       overflowY="hidden"
+      >
+         
+      </Box>
+    </Box>
+  );
 };
 
 export default MyChats;

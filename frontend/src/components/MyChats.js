64e4248b09074d924824617f1c49e6aeae1a4d6e
Abhisek Biswas <techsupport@drivado.com>
2023-03-18 17:57:43 +0530

    Second Commit

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useToast } from "@chakra-ui/react";
+import axios from "axios";
+import React, { useState,useEffect } from "react";
+import { ChatState } from "../Context/ChatProvider";
 
 const MyChats = () => {
-  return (
-    <div>MyChats</div>
-  )
-}
+  const [loggedUser, setLoggedUser] = useState();
+  const { user, selectedChat, setSelectedChat, chats, setChats } = ChatState();
 
-export default MyChats
+  const toast = useToast();
+
+  const fetchChats = async () => {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get("http://localhost:5000/api/chat", config);
+      // console.log(data);
+      setChats(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the chats",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
+
+  useEffect(()=>{
+    setLoggedUser(JSON.parse(localStorage.getItem("userInfo")))
+    fetchChats()
+  },[])
+  return <div>MyChats</div>;
+};
+
+export default MyChats;
--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -11,31 +11,111 @@ import {
   MenuDivider,
   Drawer,
   DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  Input,
+  useToast,
+  Spinner,
 } from "@chakra-ui/react";
 import { useDisclosure } from "@chakra-ui/hooks";
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { BellIcon,ChevronDownIcon} from '@chakra-ui/icons'
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { ChatState } from "../../Context/ChatProvider";
 import ProfileModal from "./ProfileModal";
 import { useNavigate } from "react-router-dom";
-
+import axios from "axios";
+import ChatLoading from "../ChatLoading";
+import UserListItem from "../userAVatar/UserListItem";
 
 const SideDrawer = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const {user} = ChatState();
+  const { user, setSelectedChat, chats, setChats } = ChatState();
 
-  const logoutHandler = ()=>{
-      localStorage.removeItem("userInfo");
-      navigate("/");
-  }
-  return (    
+  const toast = useToast();
+  const logoutHandler = () => {
+    localStorage.removeItem("userInfo");
+    navigate("/");
+  };
+
+  const handleSearch = async () => {
+    if (!search) {
+      toast({
+        title: "Please Enter Anything Inside Search",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+      return;
+    }
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const response = await axios.get(
+        `http://localhost:5000/api/user?search=${search}`,
+        config
+      );
+
+      const data = response.data;
+      // console.log(data);
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured !",
+        description: "Failed To Load the Search Result",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+    }
+  };
+
+  const accessChat = async (userId) => {
+    try {
+      setLoadingChat(true);
+      const config = {
+        "Content-Type": "application/json",
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(
+        "http://localhost:5000/api/chat",
+        config,
+        { userId }
+      );
+
+      if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
+      
+      setSelectedChat(data);
+      setLoadingChat(false);
+      onClose();
+    } catch (error) {
+      toast({
+        title: "Error fetching the chat",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
+  return (
     <>
       <Box
         display="flex"
@@ -47,7 +127,7 @@ const SideDrawer = () => {
         borderWidth="5px"
       >
         <Tooltip label="Search User To Chat" hasArrow placement="bottom-end">
-          <Button variant="ghost">
+          <Button variant="ghost" onClick={onOpen}>
             <FontAwesomeIcon icon={faMagnifyingGlass} />
             <Text display={{ base: "none", md: "flex" }} px="4">
               Search User
@@ -60,31 +140,57 @@ const SideDrawer = () => {
         <div>
           <Menu>
             <MenuButton p={1}>
-              <BellIcon fontSize="2xl" m={1}/>
+              <BellIcon fontSize="2xl" m={1} />
             </MenuButton>
           </Menu>
 
           <Menu>
-            <MenuButton as={Button} rightIcon={<ChevronDownIcon/>}>
-              <Avatar size="sm" cursor="pointer" name={user.name} src={user.pic}/>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                name={user.name}
+                src={user.pic}
+              />
             </MenuButton>
             <MenuList>
-                <ProfileModal user={user}>
-                <MenuItem > My Profile </MenuItem>
-                </ProfileModal>
-                <MenuDivider/>
-                <MenuItem onClick={logoutHandler}> Logout </MenuItem>
-              </MenuList>
+              <ProfileModal user={user}>
+                <MenuItem> My Profile </MenuItem>
+              </ProfileModal>
+              <MenuDivider />
+              <MenuItem onClick={logoutHandler}> Logout </MenuItem>
+            </MenuList>
           </Menu>
         </div>
       </Box>
-      <Drawer
-      isOpen={isOpen}
-      placement='left'
-      onClose={onClose}
-      >
-        <DrawerOverlay/> 
-
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Search User</DrawerHeader>
+          <DrawerBody>
+            <Box display="flex" pb={2}>
+              <Input
+                placeholder="Search By Name Or Email"
+                mr={2}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <Button onClick={handleSearch}>Go</Button>
+            </Box>
+            {loading ? (
+              <ChatLoading />
+            ) : (
+              searchResult?.map((user) => (
+                <UserListItem
+                  key={user._id}
+                  user={user}
+                  handleFunction={() => accessChat(user._id)}
+                />
+              ))
+            )}
+            {loadingChat && <Spinner ml="auto" d="flex" />}
+          </DrawerBody>
+        </DrawerContent>
       </Drawer>
     </>
   );
